4aceae277549004bc32376c7604ab4d58106b57e
Atul Patel <49906815+geekyatul@users.noreply.github.com>
2022-01-25 14:50:03 +0530

    Time: 1561 ms, Memory: 40.4 MB - LeetHub

---
+++ implement-strstr/implement-strstr.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        if(needle.isEmpty())
+            return 0;
+        
+         if(haystack.contains(needle))
+         {
+             return haystack.indexOf(needle);
+             
+         }
+        return -1;
+        
+        
+    }
+}
