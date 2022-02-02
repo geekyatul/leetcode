2033aa3fcf3c171b7067193dadfc419286f95264
Atul Patel <49906815+geekyatul@users.noreply.github.com>
2022-02-02 15:47:27 +0530

    Time: 1 ms (71.82%), Space: 51 MB (11.29%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,14 +1,55 @@
 class Solution {
     public int missingNumber(int[] nums) {
      
-        int xor=nums.length;
+//         int xor=nums.length;
         
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             xor=xor^i;
+//             xor=xor^nums[i];
+//         }
+//         return xor;
+        
+        int missingNumber=nums.length;
+        
+        int end=0;
+        while(end<nums.length)
+        {
+            int num=nums[end];
+            if(num==nums.length)
+            {
+                end++;
+                continue;
+            }
+               
+            if(num!=end)
+            {
+                swap(nums,end,num);
+            }
+            else
+                end++;
+        }
+        
+        int miss=nums.length;
         for(int i=0;i<nums.length;i++)
         {
-            xor=xor^i;
-            xor=xor^nums[i];
+            if(i!=nums[i])
+            {
+                 miss=i;
+                break;
+                
+            }
+               
+            
         }
-        return xor;
-      
+        
+        return  miss==nums.length?nums.length:miss;
+    }
+    
+    public void swap(int arr[],int start,int end)
+    {
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
     }
 }
