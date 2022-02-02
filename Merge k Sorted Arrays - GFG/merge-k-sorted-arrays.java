211bd38af990c523ad1e94bf04fb81b74e12ff40
Atul Patel <49906815+geekyatul@users.noreply.github.com>
2022-02-02 18:56:37 +0530

    Added solution - LeetHub

---
+++ Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.java
@@ -0,0 +1,83 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+class GFG{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t > 0){
+			int n = sc.nextInt();
+			int[][] a = new int[n][n];
+			for(int i = 0; i < n; i++)
+				for(int j = 0; j < n; j++)
+					a[i][j] = sc.nextInt();
+			Solution T = new Solution();
+			ArrayList<Integer> arr= T.mergeKArrays(a, n);
+			for(int i = 0; i < n*n ; i++)
+			    System.out.print(arr.get(i)+" ");
+		    System.out.println();
+		    
+		    t--;
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    //Function to merge k sorted arrays.
+    public static ArrayList<Integer> mergeKArrays(int[][] arr,int K) 
+    {
+        // Write your code here.
+        ArrayList<Integer> ans=new ArrayList<>();
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        
+        for(int i=0;i<K;i++)
+        {
+            for(int j=0;j<arr[i].length;j++)
+            {
+                pq.offer(arr[i][j]);
+            }
+            
+        }
+        while(!pq.isEmpty())
+        {
+            ans.add(pq.poll());
+        }
+        
+        return ans;
+        
+        
+        
+    }
+    
+    
+}
+
+class Pair implements Comparable<Pair>
+{
+   public int whichArray;
+   public int currentIndex;
+   public int element;
+   
+   public Pair(int a,int b,int c)
+   {
+       whichArray=a;
+       currentIndex=b;
+       element=c;
+   }
+   
+   public int compareTo(Pair p)
+   {
+       return this.element-p.element;
+   }
+    
+}
