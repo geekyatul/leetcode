68c733d8e380b4ae2cdefdf0cc550c85d6599559
Atul Patel <49906815+geekyatul@users.noreply.github.com>
2022-02-05 22:52:24 +0530

    Time: 5 ms (88.06%), Space: 58.8 MB (60.70%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        //I will use recursion to solve the problem
+        //base cases 
+        if(head==null || head.next==null)
+            return true;
+        
+        //First we have to find the middle element 
+        ListNode middle=middleNode(head);
+          ListNode newList=middle.next;
+           middle.next=null;
+       ListNode reverseHead=reverse(newList);
+        //Now we can iterate 
+        ListNode first=head;
+        ListNode second=reverseHead;
+        while(second!=null)
+        {
+            if(first.val!=second.val)
+                return false;
+            first=first.next;
+            second=second.next;
+        }
+        
+        return true;
+        
+        
+        
+    }
+    
+    public ListNode middleNode(ListNode head)
+    {
+        if(head==null || head.next==null)
+            return head;
+        
+        ListNode slow=head;
+        ListNode fast =head;
+        while(fast.next!=null && fast.next.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+
+        
+    }
+    
+    public ListNode reverse(ListNode middle)
+    {
+        ListNode current=middle;
+        ListNode prev=null;
+        
+        while(current!=null)
+        {
+            ListNode next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
+        }
+        return prev;
+    }
+    
+    
+}
