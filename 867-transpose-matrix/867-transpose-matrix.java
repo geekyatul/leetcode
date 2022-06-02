5e284012251c2068bff15338d05e6f3a91fc6e9d
Atul Patel <49906815+geekyatul@users.noreply.github.com>
2022-06-02 23:35:28 +0530

    Time: 1 ms (52.16%), Space: 48.6 MB (37.47%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.java
+++ 867-transpose-matrix/867-transpose-matrix.java
@@ -1,8 +1,6 @@
 class Solution {
     public int[][] transpose(int[][] matrix) {
         
-        //easy problem
-        //transponse means fillping from the digonal 
         int rowLength=matrix.length;
         int colLength=matrix[0].length;
         int resMatrix[][]=new int[colLength][rowLength];
@@ -20,10 +18,5 @@ class Solution {
         
     }
     
-    public void swap(int[][] matrix,int row,int col)
-    {
-        int temp=matrix[row][col];
-        matrix[row][col]=matrix[col][row];
-        matrix[col][row]=temp;
-    }
+   
 }
